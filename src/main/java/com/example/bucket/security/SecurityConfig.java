6b258b7b8125992d816104f5231bfa721badd401
matejlamza <matej.lamza@asynclabs.co>
2020-11-18 18:01:44 +0100

    Add security admin role user role

---
+++ src/main/java/com/example/bucket/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.bucket.security;
+
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin")
+                .password(passwordEncoder().encode("adminpass"))
+                .roles("ADMIN", "USER")
+                .and()
+                .withUser("user")
+                .password(passwordEncoder().encode("userpass"))
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .antMatchers("/user/login-user").anonymous()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/user/login-user")
+                .defaultSuccessUrl("/login", true)
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login?logout");
+
+    }
+}
